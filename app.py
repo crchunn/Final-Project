7cf57025c43c549a4fa2d604cc47492f279328e8
Carrie Chunn <crchunn@gmail.com>
2020-10-17 12:26:33 -0500

    adding flask

--- app.py
+++ app.py
@@ -1,11 +1,62 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+import argparse
+import logging
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+logger = logging.getLogger('examples.artist_albums')
+logging.basicConfig(level='INFO')
+
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 app = Flask(__name__)
 
-@app.route('/<webpage.html>/')
-def render_static(page_name):
-    return render_template('%s.html' % page_name)</string:page_name>
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Gets albums from artist')
+    parser.add_argument('-a', '--artist', required=True,
+                        help='Name of Artist')
+    return parser.parse_args()
+
+
+def get_artist(name):
+    results = sp.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        return items[0]
+    else:
+        return None
+
+
+def show_artist_albums(artist):
+    albums = []
+    results = sp.artist_albums(artist['id'], album_type='album')
+    albums.extend(results['items'])
+    while results['next']:
+        results = sp.next(results)
+        albums.extend(results['items'])
+    seen = set()  # to avoid dups
+    albums.sort(key=lambda album: album['name'].lower())
+    for album in albums:
+        name = album['name']
+        if name not in seen:
+            logger.info('ALBUM: %s', name)
+            seen.add(name)
+
+
+def main():
+    args = get_args()
+    artist = get_artist(args.artist)
+    if artist:
+        show_artist_albums(artist)
+    else:
+        logger.error("Can't find artist: %s", artist)
 
 if __name__ == '__main__':
     app.run()
